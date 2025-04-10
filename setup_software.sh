400c88034a4d4d50fd1af71a812cde0c3e39b723
Dean Stamler <dev@stamler.ca>
2025-04-10 18:03:38 -0400

    evolution install and configure script

---
+++ setup_software.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt install -y evolution evolution-ews
+
+# Configure Evolution to use Microsoft Exchange as follows:
+# Open Evolution and go to Edit -> Preferences -> Mail Accounts -> Add
+# Press Next, enter your name and email address
+# Uncheck "Look up mail server details", click Next
+# Select "Exchange Web Services" from Server Type
+# Enter https://outlook.office365.com/EWS/Exchange.asmx for Host URL
+# Select OAuth2 as Authentication Method
+# Next -> Next -> Next -> Apply
