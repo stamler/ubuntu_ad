279be3b8e9e8d6365cdeb0a3c0ac302e15f64029
Dean Stamler <dev@stamler.ca>
2025-04-10 20:14:22 -0400

    include tailscale software

--- setup_software.sh
+++ setup_software.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+# Add Tailscale Repository
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+apt update
+
 apt remove thunderbird
-apt install -y evolution evolution-ews
+apt install -y evolution evolution-ews tailscale
 
 # Configure Evolution to use Microsoft Exchange as follows:
 # Open Evolution and go to Edit -> Preferences -> Mail Accounts -> Add
