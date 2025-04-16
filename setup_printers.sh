d36332be121cf3c6b9f30bb69e33375c422f8929
Dean Stamler <dev@stamler.ca>
2025-04-16 13:18:58 -0400

    Create setup_printers.sh

---
+++ setup_printers.sh
@@ -0,0 +1,6 @@
+lpadmin -p QuebecEast -E -v ipps://10.191.80.8:443/ipp/print -m everywhere
+lpadmin -p QuebecWest -E -v ipps://10.191.80.14:443/ipp/print -m everywhere
+lpadmin -p QuebecPlotter -E -v ipps://10.191.80.60:443/ipp/print -m everywhere
+lpadmin -p Kingston -E -v ipps://10.191.80.33:443/ipp/print -m everywhere
+lpadmin -p YongeMain -E -v ipps://10.191.80.11:443/ipp/print -m everywhere
+lpadmin -p YongeSecond -E -v ipps://10.191.80.252:443/ipp/print -m everywhere
